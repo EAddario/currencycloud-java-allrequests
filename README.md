c17cb72ffce497ea587ee5dcea59db317924c017
Ed <29247825+EAddario@users.noreply.github.com>
2018-05-28 14:24:33 +0100

    Initial commit

---
+++ README.md
@@ -0,0 +1,4 @@
+# java-allrequests
+Executes all API calls available in the [Currencycloud Java SDK][sdk]
+
+[sdk]: https://github.com/CurrencyCloud/currencycloud-java
---
+++ src/test/java/org/addario/AllRequestsTest.java
@@ -0,0 +1,20 @@
+package org.addario;
+
+import org.junit.Test;
+
+import static org.junit.Assert.assertTrue;
+
+/**
+ * Unit test for AllRequests.
+ */
+public class AllRequestsTest
+{
+    /**
+     * Rigorous Test :-)
+     */
+    @Test
+    public void shouldAnswerWithTrue()
+    {
+        assertTrue( true );
+    }
+}
